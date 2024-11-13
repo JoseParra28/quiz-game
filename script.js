2dfbaa042a8f83d883b235e63267fcbca50ae6e7
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-11-13 15:04:52 +0000

    input value

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let randomNum = Math.trunc(Math.random(0) * 10) + 1;
 const number = document.querySelector(".number");
 const guess = document.querySelector(".guess").value;
-const check = document.querySelector(".check")
+const check = document.querySelector(".check");
+const message = document.querySelector(".message")
 
 number.textContent = randomNum
 
 check.addEventListener("click", function(){
     const guess = document.querySelector(".guess").value;
+    if (!guess){
+        message.textContent = "There's no numbers here"
+    }
     console.log("cicked")
     console.log(guess)
 })
