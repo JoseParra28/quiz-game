735bd9da03c462a32b6c89feb9f9e10b00bfd6cb
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-11-13 14:41:25 +0000

    random num

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test")
+let randomNum = Math.trunc(Math.random(0) * 10) + 1;
+const number = document.querySelector(".number");
+const guess = document.querySelector(".guess").value;
+const check = document.querySelector(".check")
+
+number.textContent = randomNum
+
+check.addEventListener("click", function(){
+    const guess = document.querySelector(".guess").value;
+    console.log("cicked")
+    console.log(guess)
+})
