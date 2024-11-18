0b5ad5d58beba33215fe67cc8da8e1ec6e96895e
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-11-18 15:17:07 +0000

    buttons

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-let randomNum = Math.trunc(Math.random(0) * 10) + 1;
-const number = document.querySelector(".number");
-const guess = document.querySelector(".guess").value;
-const check = document.querySelector(".check");
-const message = document.querySelector(".message")
+// let randomNum = Math.trunc(Math.random(0) * 10) + 1;
+// const number = document.querySelector(".number");
+// const guess = document.querySelector(".guess").value;
+// const check = document.querySelector(".check");
+// const message = document.querySelector(".message")
 
-number.textContent = randomNum
+// number.textContent = randomNum
 
-check.addEventListener("click", function(){
-    const guess = document.querySelector(".guess").value;
-    if (!guess){
-        message.textContent = "There's no numbers here"
-    }
-    console.log("cicked")
-    console.log(guess)
+// check.addEventListener("click", function(){
+//     const guess = document.querySelector(".guess").value;
+//     if (!guess){
+//         message.textContent = "There's no numbers here"
+//     }
+//     console.log("cicked")
+//     console.log(guess)
+// })
+
+const questions = document.querySelector(".question");
+let questionsBtn = document.querySelector(".answer-btn1");
+let answers = ["Jose", "Carlos", "Pepe", "Sebastian"]
+
+questionsBtn.addEventListener("click", function(){
+    selectAns()
 })
+
+
+function selectAns(){
+    for (let i = 0; i <answers.length; i++){
+        let buttons =  document.createElement("button");
+        buttons.innerHTML.answers[i];
+        buttons.appendChild(answers)
+     }
+}
+
+// var btn = document.querySelector(".answer-btn1");
+// var optionText = ["Button 1", "Button 2", "Button 3", "Button 4"];
+
+// btn.addEventListener("click", function(){
+//   buttonSelect();
+// })
+
+// function buttonSelect() {
+//   for(var i = 0; i < optionText.length; i++){
+//     var option = document.createElement("button");
+//     option.innerHTML = optionText[i];
+//     document.body.appendChild(option);
+//   }
+// }
